3878e06636ea088f2b7223e5c9298a6d2f40b7b3
Abhinav Kumar <b18099@students.iitmandi.ac.in>
2020-10-15 19:54:26 +0530

    input number validation checks added.

--- sqrt.c
+++ sqrt.c
@@ -7,13 +7,25 @@ positive number. */
 
 // main function takes arguments from the terminal
 // and calculate square root using sqrt function
+
+
 int main(int argc, char* argv[]) {
 	
 	if (argc != 2) {
 		printf("Usage: sqrt input\n\n");
 		exit(-1);
 	}
-
+	
+	//validation to check the given argument is number or not 
+	long num = 0;
+	char* endp = NULL;
+	num = strtol(argv[1],&endp,0);
+	if (endp == NULL || *endp != (char)0)
+    { 
+	   printf("program argument %s is not a number\n", argv[1]);
+       exit (-1);
+	}; 
+	
 	int input = atoi(argv[1]);
 	printf("Sqrt of %d is %f\n",input,sqrt(input));
 	printf("End of program.Exiting.\n");
